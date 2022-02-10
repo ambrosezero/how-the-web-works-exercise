fae0253e0542928cb0942b84f21f7b8de0a69148
Lance Shaw <AmbroseSouth@outlook.com>
2022-02-10 11:21:11 -0600

    assignment complete

---
+++ app.js
@@ -0,0 +1,25 @@
+// http is a protocol for communicating between different computers
+
+// url stands for Uniform Resource Locator, and  is a way finding a server, and 
+// requesting information back from it.
+
+//  a DNS is a system that maintains a list of domain names and their applicable IP addresses,
+// and redirects traffic to those IP addresses.
+
+//  a format for specifying what information you are asking for from a server; it begins with a 
+// ? then is followed by key value pairs
+
+//  GET is a way of requesting information from a server
+//  POST is a way of sending data to, and changing data on, a server.
+
+// a request, set in the http protocol, for inormation from a server
+
+// a response from a server to an http request. it may or may not contain the requested information
+
+// provide information about the nature of the response/request
+// Accept, cookie, cache-control
+// Language
+
+// using a DNS, the domain name is converted into an IP address, then sends a request for information
+// to that ip address. the server responds with that information, and you browser makes a DOM from that
+//  HTML, as well as requests other files as necessary, ie images, in the form of futher http requests
